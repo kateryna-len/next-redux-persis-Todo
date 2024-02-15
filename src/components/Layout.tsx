78b7c390dcfd60084b9ad922a23647183c30c920
kateryna-len <lenkaterynna057@gmail.com>
2024-02-15 16:36:05 +0200

    created  project todo

---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { persistor, store } from "@/redux/store";
+import React from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+interface Props {
+  children: React.ReactElement;
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>{children}</PersistGate>
+    </Provider>
+  );
+};
+
+export default Layout;
